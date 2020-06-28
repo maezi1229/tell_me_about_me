99e3a6ecc4156dcbfea2a7b7bfaf4783215c9f1d
maezi1229 <takuya.maejima1229@gmail.com>
2020-06-28 18:26:50 +0900

    ADD

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.250.44.242', user: 'ec2-user', roles: %w{app db web}
+server '54.95.147.204', user: 'ec2-user', roles: %w{app db web}
